acffd7fed13901cf02030c5e90c706e78b803102
Dao An <daoan1412@gmail.com>
2018-12-22 02:01:54 +0700

    ok

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const Class = require("./index");
 const fs = require("fs");
 let obj = new Class();
+const SafariContentBlockerConverter = require("./filter/rules/converter");
 
-let FileData = fs.readFileSync("./2.txt").toString();
-fs.writeFileSync('./data.json', JSON.stringify(obj.convertFileDataToRulesData(FileData)));
+// let FileData = fs.readFileSync("./easylist.txt").toString();
+// fs.writeFileSync('./data.json', JSON.stringify(obj.convertFileDataToRulesData(FileData)));
 
-obj = new Class(require('./data.json'));
-console.log(
-  obj.isUrlAds("https://c.amazon-adsystem.com/aax2/apstag.js")
+// obj = new Class(require('./data.json'));
+// console.log(
+//   obj.isUrlAds("https://amd-ssl.cdn.turner.com/cnn/big/ads/2018/07/13/KoreanAirGlobal_30s_EN_v2_576x324.mp4")
+// );
+const safariJSON = SafariContentBlockerConverter.convertArray(
+  require("./data.json"),
+  50000,
+  true
 );
+if (safariJSON.converted.length > 0) {
+  fs.writeFileSync(
+    "./adaway.json",
+
+    safariJSON.converted.replace(/(\t|\n|)/gm, "")
+  );
+  console.log("=====");
+}
