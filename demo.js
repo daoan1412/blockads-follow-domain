9ef35da26bf9a98b33badb66f11d825f1bd97db5
Dao An <daoan1412@gmail.com>
2019-06-03 13:36:54 +0700

    1:36

--- demo.js
+++ demo.js
@@ -3,18 +3,18 @@ const adsClass = require("./index");
 const adsInstance = new adsClass();
 const fs = require("fs");
 
-const baseUrl = "https://filters.adtidy.org/extension/chromium/filters/";
-const adsList = ["2_optimized", "3", "4", "11", "12", "14"];
-let content = "";
+// const baseUrl = "https://filters.adtidy.org/extension/chromium/filters/";
+// const adsList = ["2_optimized", "3", "4", "11", "12", "14"];
+// let content = "";
 
-for (let file of adsList) {
-  content += downloadFileSync(`${baseUrl}${file}.txt`) + "\n";
-}
+// for (let file of adsList) {
+//   content += downloadFileSync(`${baseUrl}${file}.txt`) + "\n";
+// }
 
-fs.writeFileSync(
-  "./data.json",
-  JSON.stringify(adsInstance.convertFileDataToRulesData(content))
-);
+// fs.writeFileSync(
+//   "./data.json",
+//   JSON.stringify(adsInstance.convertFileDataToRulesData(content))
+// );
 
 obj = new adsClass(require("./data.json"));
 console.log(
@@ -23,18 +23,18 @@ console.log(
   )
 );
 
-const cssDomainRules = adsInstance.convertRulesDataToDomainCssRules(
-  require("./data.json")
-);
-console.log(`cssDomainRules: ${cssDomainRules.length}`);
-fs.writeFileSync("./cssDomainRules.json", JSON.stringify(cssDomainRules));
+// const cssDomainRules = adsInstance.convertRulesDataToDomainCssRules(
+//   require("./data.json")
+// );
+// console.log(`cssDomainRules: ${cssDomainRules.length}`);
+// fs.writeFileSync("./cssDomainRules.json", JSON.stringify(cssDomainRules));
 
-const cssCommonRules = adsInstance.convertRulesDataToCommonCssRules(
-  require("./data.json")
-);
-console.log(`cssCommonRules: ${cssCommonRules.length}`);
-fs.writeFileSync("./cssCommonRules.json", JSON.stringify(cssCommonRules));
+// const cssCommonRules = adsInstance.convertRulesDataToCommonCssRules(
+//   require("./data.json")
+// );
+// console.log(`cssCommonRules: ${cssCommonRules.length}`);
+// fs.writeFileSync("./cssCommonRules.json", JSON.stringify(cssCommonRules));
 
-const cssCommonHidden = new adsClass().getCssCommonRules(require("./data.json"));
-fs.writeFileSync("./common.css", cssCommonHidden);
+// const cssCommonHidden = new adsClass().getCssCommonRules(require("./data.json"));
+// fs.writeFileSync("./common.css", cssCommonHidden);
 
